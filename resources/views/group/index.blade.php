17de20b3760212ffd4dd1b20c0d210c71d6184ba
”TanakaRyunosuke” <ryu0826tanaka@gmail.com>
2019-09-08 03:34:13 +0000

    Edit group index

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -1,16 +1,42 @@
 {{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'チャットグループ一覧')
+{{-- admin.blade.phpの@yield('title')に'チャットグループ一覧'を埋め込む --}}
+@section('title', 'チャット一覧')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>チャットグループ一覧</h2>
-            </div>
+            <h2>チャット一覧</h2>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">id</th>
+                  <th scope="col">チャットグループ名</th>
+                  <th scope="col">詳細</th>
+                  <th scope="col">Last Comment</th>
+                  <th scope="col">Leave one's group</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                        <td>名前</td>
+                        <td>
+                          <div>
+                            <a href="#">チャット画面へ</a>
+                          </div>
+                        </td>
+                        <td>2019/08/09 12:49</td>
+                        <td>
+                          <div>
+                            <a href="#">Exit</a>
+                          </div>
+                        </td>
+                    </tr>
+              </tbody>
+            </table>
         </div>
     </div>
-@endsection 
+@endsection
